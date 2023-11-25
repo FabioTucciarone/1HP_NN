99af7e5421669583536cf5ab80263432e3809a83
Fabio Tucciarone <fabiotucciarone@gmail.com>
2023-11-25 16:25:18 +0100

    Fehler behoben

--- data_stuff/utils.py
+++ data_stuff/utils.py
@@ -29,6 +29,7 @@ class SettingsTraining:
     finetune: bool = False
     model: str = None
     test: bool = False
+    case_2hp: bool = False
     visualize: bool = False
     
     def __post_init__(self):
